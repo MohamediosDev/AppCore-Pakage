594053eb2eb3ae9244fa1b50296f5fb8de7d0494
mohamed Sayed <mrdiscover22@gmail.com>
2023-02-05 16:34:06 +0200

    fix: files in pakage

---
+++ Sources/AppCore/TableView+Ext.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mohamed sayed on 05/02/2023.
+//
+
+import UIKit
+
+
+public extension UITableView {
+    
+    func registerCellNib<Cell: UITableViewCell>(cellClass: Cell.Type){
+        self.register(UINib(nibName: String(describing: Cell.self), bundle: nil), forCellReuseIdentifier: String(describing: Cell.self))
+    }
+
+
+    func dequeue<Cell: UITableViewCell>() -> Cell{
+        let identifier = String(describing: Cell.self)
+        
+        guard let cell = self.dequeueReusableCell(withIdentifier: identifier) as? Cell else {
+            fatalError("Error in cell")
+        }
+        return cell
+    }
+}
